ed60187038ee8986e0b458ecab10cf0994024392
Pan Wenbin <panwenbin@gmail.com>
2019-08-10 07:21:30 +0800

    use init in router

--- routers/router.go
+++ routers/router.go
@@ -5,9 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var r *gin.Engine
+
 func Load() *gin.Engine {
-	r := gin.Default()
 	r.GET("/hello", actions.Hello)
 
 	return r
+}
+
+func init() {
+	r = gin.Default()
 }
