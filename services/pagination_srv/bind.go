1987bc838c9014a95b65cb8117e6c8a1211ecaf1
Pan Wenbin <panwenbin@gmail.com>
2020-06-24 09:45:31 +0800

    remove count in pagination_srv

--- services/pagination_srv/bind.go
+++ services/pagination_srv/bind.go
@@ -7,19 +7,17 @@ import (
 )
 
 // Bind binds pagination queries in request to a gorm Query
-func Bind(c *gin.Context, query *gorm.DB) (int, *gorm.DB) {
+func Bind(c *gin.Context, query *gorm.DB) *gorm.DB {
 	pagi := requests.Pagination{
 		Page:    1,
 		PerPage: 10,
 	}
 	_ = c.BindQuery(&pagi)
-	total := 0
-	query.Count(&total)
 
 	query = query.Offset((pagi.Page - 1) * pagi.PerPage).Limit(pagi.PerPage)
 	if pagi.Order != "" {
 		query = query.Order(pagi.Order)
 	}
 
-	return total, query
+	return query
 }
