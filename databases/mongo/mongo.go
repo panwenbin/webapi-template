667e97d1d0f8af5d02f53891944d41a09554a5d9
Pan Wenbin <panwenbin@gmail.com>
2020-07-16 11:22:38 +0800

    change mongo driver to globalsign/mgo

--- databases/mongo/mongo.go
+++ databases/mongo/mongo.go
@@ -2,16 +2,12 @@ package mongo
 
 import (
 	"app/settings"
-	"context"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"github.com/globalsign/mgo"
 	"log"
 	"os"
-	"time"
 )
 
-var Mongo *mongo.Client
+var Mongo *mgo.Session
 
 var (
 	MongodbUri string
@@ -24,21 +20,9 @@ func init() {
 
 	MongodbUri = os.Getenv("MONGODB_URI")
 
-	client, err := mongo.NewClient(options.Client().ApplyURI(MongodbUri))
+	var err error
+	Mongo, err = mgo.Dial(MongodbUri)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	ctx, _ = context.WithTimeout(context.Background(), 2*time.Second)
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatalln(err)
-	}
-	Mongo = client
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,7 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/jinzhu/gorm v1.9.14
 	github.com/joho/godotenv v1.3.0
-	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
-	go.mongodb.org/mongo-driver v1.3.4
 )
